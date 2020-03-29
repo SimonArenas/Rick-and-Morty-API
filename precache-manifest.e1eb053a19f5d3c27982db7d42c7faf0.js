ca90b0bb0fc091e7b8ac5ad3d58de7a3cb07fad3
SimonArenas <simon.arenas98@gmail.com>
2020-03-28 21:21:22 -0600

    Updates

---
+++ precache-manifest.e1eb053a19f5d3c27982db7d42c7faf0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e78da69dde5e49bfbe3ff5302c6a39f",
+    "url": "/Rick-and-Morty-API/index.html"
+  },
+  {
+    "revision": "250dd734c26cb9253055",
+    "url": "/Rick-and-Morty-API/static/css/2.6e4caf0c.chunk.css"
+  },
+  {
+    "revision": "8fd6a7d3f1ccad6f8173",
+    "url": "/Rick-and-Morty-API/static/css/main.6444e38c.chunk.css"
+  },
+  {
+    "revision": "250dd734c26cb9253055",
+    "url": "/Rick-and-Morty-API/static/js/2.46c392e4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Rick-and-Morty-API/static/js/2.46c392e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fd6a7d3f1ccad6f8173",
+    "url": "/Rick-and-Morty-API/static/js/main.d45d2171.chunk.js"
+  },
+  {
+    "revision": "fcd9a407edf4e3fa2b42",
+    "url": "/Rick-and-Morty-API/static/js/runtime-main.10cb69f4.js"
+  },
+  {
+    "revision": "960a1abf7958f13d2617fba06264ae60",
+    "url": "/Rick-and-Morty-API/static/media/loader.960a1abf.gif"
+  },
+  {
+    "revision": "de82c64b0013c1e2a22f5a8fb26f04f4",
+    "url": "/Rick-and-Morty-API/static/media/logo.de82c64b.png"
+  }
+]);
